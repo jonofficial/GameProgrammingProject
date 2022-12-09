10f3fae819e23e3360754ecf18fd641b46b4798b
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-12-08 22:53:54 -0300

    FEAT - settings

--- Assets/Scenes/MainMenu/MainMenu.cs
+++ Assets/Scenes/MainMenu/MainMenu.cs
@@ -1,7 +1,51 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+
+    [SerializeField] private protected Dropdown dropResolutions, dropQualitys;
+    [SerializeField] private protected Toggle toggleWindow;
+
+    private List<string> resolutions = new List<string>();
+    private List<string> qualitys = new List<string>();
+
+    private void Start() {
+        dropResolutions.ClearOptions();
+        dropQualitys.ClearOptions();
+         
+        Resolution[] arrResolutions = Screen.resolutions;
+        
+        Debug.Log(arrResolutions);
+        for(int i = 0; i < arrResolutions.Length; i++) Debug.Log(i + ": " + arrResolutions[i]);
+
+        foreach(Resolution r in arrResolutions) resolutions.Add(string.Format("{0} X {1}", r.width, r.width));
+
+        dropResolutions.AddOptions(resolutions);
+        dropResolutions.value = (resolutions.Count - 1);
+
+        qualitys = QualitySettings.names.ToList<string>();
+        dropQualitys.AddOptions(qualitys);
+        dropQualitys.value = QualitySettings.GetQualityLevel();
+    }
+
+    public void SetResolution() {
+        string[] res = resolutions[dropResolutions.value].Split('X');
+        Screen.SetResolution(Convert.ToInt16(res[0].Trim()), Convert.ToInt16(res[1].Trim()), Screen.fullScreen);
+    }
+    public void SetQuality() {
+        QualitySettings.SetQualityLevel(dropQualitys.value, true);
+    }
+
+    public void SetWindowMode() {
+        if(toggleWindow.isOn) Screen.fullScreen = false; 
+        else Screen.fullScreen = true;
+    }
+
     public void StartGame() {
         SceneManager.LoadSceneAsync(1);
     }
